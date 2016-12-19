e4dd9529a9b2c754cb4f0d661171979a372228f7
Stephan Ahlf <stephan.ahlf@googlemail.com>
2016-12-19 22:15:29 +0100

    add error handler

--- app/app.js
+++ app/app.js
@@ -1,28 +1,40 @@
-window.resizeScrollViews = function resizeScrollViews () {
-	var w = window,
-	d = document,
-	e = d.documentElement,
-	g = d.getElementsByTagName('body')[0],
-	//    x = w.innerWidth || e.clientWidth || g.clientWidth,
-	y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+try{
+	window.resizeScrollViews = function resizeScrollViews () {
+		var w = window,
+		d = document,
+		e = d.documentElement,
+		g = d.getElementsByTagName('body')[0],
+		//    x = w.innerWidth || e.clientWidth || g.clientWidth,
+		y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-	var el = document.getElementsByClassName("clusterize-scroll");
-	for (var i = 0; i < el.length; i++) {
-		el[i].setAttribute("style","max-height:" + (y - 80) + "px");
-	}
-};
+		var el = document.getElementsByClassName("clusterize-scroll");
+		for (var i = 0; i < el.length; i++) {
+			el[i].setAttribute("style","max-height:" + (y - 80) + "px");
+		}
+	};
 
-window.addEventListener("resize", window.resizeScrollViews);
+	window.addEventListener("resize", window.resizeScrollViews);
 
-var ApplicationController = require("./app.controller.js");
-var applicationController = new ApplicationController({nodeModulesFolder: __dirname });
-window.Clusterize = require("clusterize.js");
+	var ApplicationController = require("./app.controller.js");
+	var applicationController = new ApplicationController({nodeModulesFolder: __dirname });
+	window.Clusterize = require("clusterize.js");
 
-window.onload = function appLoad() {
-	if (applicationController.initialize()){
+	window.onload = function appLoad() {
+		if (applicationController.initialize()){
 
-		applicationController.events.emit("init-main-menu", applicationController);
-		applicationController.events.emit("init-gui", applicationController);
-		applicationController.events.on("core-init-done", function(){});
-	}
-};
+			applicationController.events.emit("init-main-menu", applicationController);
+			applicationController.events.emit("init-gui", applicationController);
+			applicationController.events.on("core-init-done", function(){});
+		}
+	};
+	require('electron').remote.getCurrentWindow().setMenu(null);   
+} catch(e){
+	const {dialog} = require('electron').remote; 
+	var PrettyError = require('pretty-error');
+	var pe = new PrettyError();
+	pe.withoutColors();
+	pe.skipNodeFiles(); // this will skip events.js and http.js and similar core node files
+	pe.skipPackage('express'); // this will skip all the trace lines about express` core and sub-modules
+	var renderedError = pe.render(e); 
+	dialog.showErrorBox("title", renderedError);
+}

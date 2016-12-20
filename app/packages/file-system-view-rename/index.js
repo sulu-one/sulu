de0c50c3c473053b954b6a85d65693d05e93dde0
Stephan Ahlf <stephan.ahlf@googlemail.com>
2016-12-20 20:05:26 +0100

    prepare rename

---
+++ app/packages/file-system-view-rename/index.js
@@ -0,0 +1,42 @@
+var fs = require("fs");
+var path = require("path");
+
+var Command = function() {
+	return this;
+};
+
+Command.prototype.rename = function rename() {
+	var mkdirSync = function (path) {
+	  try {
+	    fs.mkdirSync(path);
+	  } catch(e) {
+	    if ( e.code != 'EEXIST' ) {
+	    	this.GUI.app.msg(e);
+	    };
+	  }
+	};
+	var v = this.GUI.activeView();
+	if (v){
+		const {shell} = require('electron');
+		var view = this.GUI.activeView().model;
+		var file = view.activeRowData(); 
+		var defaultText = (file ? file.name : "new_folder");
+		var self = this;
+		this.GUI.dialogs.prompt("New folder in \"" + view.path + "\"", defaultText, function(input) {
+			if (input !== undefined){ 
+				mkdirSync(path.join(view.path, input));
+				view.refresh(input);
+			}
+		});
+	} else {
+		this.GUI.app.msg("please select a file system view.");
+	}
+	return false;
+};
+
+var Plugin = function (client) {
+	this.command = new Command(); 
+	client.app.registerHotKey("f2", this.command.rename);
+};
+
+module.exports = Plugin;

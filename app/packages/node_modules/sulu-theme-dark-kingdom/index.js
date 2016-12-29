fb94595e790bd2ec49814dc92d32e285a18c2866
Stephan Ahlf <stephan.ahlf@googlemail.com>
2016-12-29 18:47:18 +0100

    submodule sulu-theme-dark-kingdom

--- app/packages/node_modules/sulu-theme-dark-kingdom/index.js
+++
@@ -1,8 +0,0 @@
-var path = require("path");
-
-var Plugin = function  (client) { 
-	client.app.loadHTML(path.join(__dirname, "dk.html"));
-	client.app.loadCSS(path.join(__dirname, "dk.css"));
-};
-
-module.exports = Plugin;
